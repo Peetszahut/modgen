3020ffed2a61daff6e899d8dbef3b8ae0e369a53
Peetszahut <peetszahut@gmail.com>
2018-08-28 16:42:48 -0700

    Highlight area to input code for feature engineering

--- modgen_main.py
+++ modgen_main.py
@@ -1,9 +1,3 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
 from modgen_utils import *
 from modgen_classifier_models import *
 import pandas as pd
@@ -16,56 +10,12 @@ from tqdm import tqdm_notebook as tqdm
 import warnings
 warnings.filterwarnings(action='ignore', category=DeprecationWarning)
 
+##################### Feature Engineering Code Goes Here #####################
 
-# In[6]:
+train = pd.read_csv("~/train.csv")
+test = pd.read_csv("~/test.csv")
 
-
-### Titanic Classifier Testing Set
-
-path = "/Users/jeromydiaz/Desktop/all/"
-train = pd.read_csv("/Users/jeromydiaz/Desktop/all/train.csv")
-test = pd.read_csv("/Users/jeromydiaz/Desktop/all/test.csv")
-
-def titleCheck(x):
-    # x[0] = name
-    # x[1] = pclass
-    important_title = ['Rev.', 'Dr.', 'Mme', 'Major', 'Lady', 'Sir.', 'Col.', 'Capt.', 'Countess']
-    for title in important_title:
-        if title in x[0] and x[1] == 1:
-            return 1 
-    return 0
-
-##### Try Me 
-test.fillna(test[test.Pclass == 3].Fare.mean(), inplace = True)
-
-train['Under18P1'] = np.where((train['Age'] < 18) & (train['Pclass'] == 1), 1, 0)
-train['Over60'] = np.where((train['Age'] > 60), 1, 0)
-train['Alone'] = np.where((train['Parch'] == 0) & (train['SibSp'] == 0), 1, 0)
-train['FareOver50'] = np.where((train['Fare'] > 50), 1, 0)
-train['1stSpecial'] = train[['Name','Pclass']].apply(titleCheck, axis = 1)
-
-test['Under18P1'] = np.where((test['Age'] < 18) & (test['Pclass'] == 1), 1, 0)
-test['Over60'] = np.where((test['Age'] > 60), 1, 0)
-test['Alone'] = np.where((test['Parch'] == 0) & (test['SibSp'] == 0), 1, 0)
-test['FareOver50'] = np.where((test['Fare'] > 50), 1, 0)
-test['1stSpecial'] = test[['Name','Pclass']].apply(titleCheck, axis = 1)
-
-# For Train
-y_train = train['Survived']
-x_train = train.drop(['PassengerId','Survived','Name','Ticket','Cabin','Embarked', 'Parch', 'SibSp'], axis=1)
-encSex = LabelEncoder()
-testEnc = encSex.fit_transform(x_train.Sex)
-x_train['Sex'] = testEnc
-x_train['Age'].fillna(x_train.Age.mean(), inplace = True)
-
-# For Test
-x_test = test.drop(['PassengerId','Name','Ticket','Cabin','Embarked', 'Parch', 'SibSp'], axis=1)
-x_test['Sex'] = encSex.fit_transform(x_test.Sex)
-x_test['Age'].fillna(x_test.Age.mean(), inplace = True)
-
-
-# In[8]:
-
+##################### Feature Engineering Code Goes Here #####################
 
 ######## Model Options ########
 ### K-Fold Options
@@ -77,7 +27,7 @@ kfold_repeats = 1
 
 ### Scaler Option: StandardScaler(), Normalizer(), MinMaxScaler(), RobustScaler()
 # If scaler_select = None, then no scaling will be done
-scaler_select = StandardScaler() 
+scaler_select = StandardScaler()
 
 ### Model creation options
 # If previousModel = False: Use new modelCreation dictionary to make models, else, use previous index to get parameters
@@ -109,18 +59,18 @@ else:
                         'svc'          : 250,
                         'decisiontree' : 500,
                         'randomforest' : 200
-                    }   
+                    }
 
-### K-Fold Cross Validation Inputs  
-if previousModel: kfold_update = False  
-if kfold_update: 
-    kfold_type = (kfold_distribution, kfold_repeats) 
-else: 
+### K-Fold Cross Validation Inputs
+if previousModel: kfold_update = False
+if kfold_update:
+    kfold_type = (kfold_distribution, kfold_repeats)
+else:
     kfold = 1
     kfold_type = None
 
 ### Scaler Function Inputs
-if scaler_select: 
+if scaler_select:
     X_train, X_test = scaleSelector(x_train, x_test, scaler_select)
 else:
     X_train, X_test = x_train, x_test
@@ -147,56 +97,56 @@ for modelSelection, numModels in modelCreation.items():
 
         if modelSelection == 'lightgbm':
             params, model = getModelLightGBM(previousModel, params)
-            
+
         elif modelSelection == 'xgboost':
             params, model = getModelXGBoost(previousModel, params)
-        
+
         elif modelSelection == 'neuralnetwork':
             params, model = getModelNeuralNetwork(previousModel, params)
-            
+
         elif modelSelection == 'lasso':
             # Lasso Model Generator
             params, model = getModelLasso(previousModel, params)
 
-        elif modelSelection == 'ridge':    
+        elif modelSelection == 'ridge':
             # Ridge Model Generator
             params, model = getModelLasso(previousModel, params)
 
-        elif modelSelection == 'knn':   
+        elif modelSelection == 'knn':
             # KNN Model Generator
             params, model = getModelKNN(previousModel, params)
 
-        elif modelSelection == 'gradboost':    
+        elif modelSelection == 'gradboost':
             # Gradient Boosting Generator
             params, model = getModelGradientBoosting(previousModel, params)
 
-        elif modelSelection == 'svc':    
+        elif modelSelection == 'svc':
             # SVC Model Generator
             params, model = getModelSVC(previousModel, params)
 
-        elif modelSelection == 'adaboost':    
+        elif modelSelection == 'adaboost':
             # AdaBoost with DecisionTreeClassifier Model Generator
             params, model = getModelAdaBoostTree(previousModel, params)
 
-        elif modelSelection == 'decisiontree':   
+        elif modelSelection == 'decisiontree':
             # DecisionTreeClassifier Model Generator
             params, model = getModelDecisionTree(previousModel, params)
 
-        elif modelSelection == 'randomforest':   
+        elif modelSelection == 'randomforest':
             # RandomForest Model Generator
             params, model = getModelRandomForest(previousModel, params)
 
 
         # Model Generation based off paramList and modelList
-        Pred_train, Pred_valid, Pred_test, kfold_DF = modelSelector(X_train, Y_train, X_valid, Y_valid, 
+        Pred_train, Pred_valid, Pred_test, kfold_DF = modelSelector(X_train, Y_train, X_valid, Y_valid,
                                                                     X_test, params, train_full, model, kfold = kfold,
                                                                     kfold_type = kfold_type,
                                                                     modeltype = modelSelection)
-        
+
         analysisDF = dataFrameUpdate(params, Y_train, Y_valid, Pred_train, Pred_valid, analysisDF, kfold_DF,
                                     kfold_update)
         if ensemble: ensembleList.append(Pred_test)
-    
+
 
 if kfold > 1:
     train_auc = analysisDF['Train Auc'].apply(lambda x: x.split('-')[0].strip()).astype('float64')
@@ -212,23 +162,8 @@ plt.show()
 if not previousModel:
     analysisDF = analysisDF.sort_values(['Valid Auc','Train Auc'], ascending = False)
     analysisDF.to_csv('//Users/jeromydiaz/Desktop/Titanic_AnalysisDF.csv')
-    
+
 # Writes final submission file
 if train_full: trainFullSubmission(Pred_test, test['PassengerId'], ensemble, ensembleList)
 
-
-# In[ ]:
-
-
 analysisDF.sort_values(['Valid Auc','Train Auc'], ascending = False)
-
-
-# In[10]:
-
-
-test1 = None
-if test1:
-    print('yes')
-else:
-    print('no')
-
